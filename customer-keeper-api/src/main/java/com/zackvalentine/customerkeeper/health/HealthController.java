759d560a65e6e93412fbfa936d65d1e58e4c866c
zackvalentine <zack.valentine22@gmail.com>
2020-11-08 01:08:18 -0600

    add health check endpoint

---
+++ customer-keeper-api/src/main/java/com/zackvalentine/customerkeeper/health/HealthController.java
@@ -0,0 +1,14 @@
+package com.zackvalentine.customerkeeper.health;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return new ResponseEntity<>("Healthy", HttpStatus.OK);
+    }
+}
