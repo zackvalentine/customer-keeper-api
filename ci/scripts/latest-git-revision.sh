37137e3c7e53a231ca3c96faaa5d3c9cbbc13e63
zackvalentine <zack.valentine22@gmail.com>
2020-11-11 16:58:33 -0600

    WIP

--- ci/scripts/latest-git-revision.sh
+++ ci/scripts/latest-git-revision.sh
@@ -2,4 +2,8 @@
 
 set -e
 
+GIT_REV_SHORT=$(git rev-parse --short HEAD)
+
+echo "Ignoring whether this directory has been updated in commit ${GIT_REV_SHORT}"
+echo ${GIT_REV_SHORT} > ../docker-env/ref
 exit 0
