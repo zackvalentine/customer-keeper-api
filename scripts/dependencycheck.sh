beb3e22f94356bfd24a993af78d99e124745bad8
Zack Valentine <zack.valentine22@gmail.com>
2020-11-29 22:47:26 -0600

    WIP

--- scripts/dependencycheck.sh
+++ scripts/dependencycheck.sh
@@ -4,7 +4,7 @@ set -e
 # ensure that your machine is set up to build software
 my_directory=$(dirname $0)
 
-for filename in $my_directory/checks/*; do
+for filename in $my_directory/*; do
   chmod +x $filename
   $filename
 done
